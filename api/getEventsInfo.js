846a51072ff64e2b3467f990100e99a6f121fab6
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-08 17:43:44 -0400

    get endpoint setup

---
+++ api/getEventsInfo.js
@@ -0,0 +1,10 @@
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Getting all 3 wedding events information",
+    }),
+  };
+
+  callback(null, response);
+};

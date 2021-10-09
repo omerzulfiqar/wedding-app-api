38c600658f3e04763954c30cf5939c7c72355fef
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-08 20:02:38 -0400

    getEventsInfo endpoint created

--- api/getEventsInfo.js
+++ api/getEventsInfo.js
@@ -1,10 +1,42 @@
+const { dynamoTableNames, headers } = require("../config");
+const AWS = require("aws-sdk");
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Getting all 3 wedding events information",
-    }),
+  const { guestCode } = event.pathParameters;
+
+  const params = {
+    TableName: dynamoTableNames.Events,
+    KeyConditionExpression: "guestCode = :guestCode",
+    ExpressionAttributeValues: {
+      ":guestCode": guestCode,
+    },
   };
 
-  callback(null, response);
+  try {
+    let result = await dynamoDB.query(params).promise();
+    const { Items } = result;
+
+    if (Items.length) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(Items),
+        headers,
+      };
+    } else {
+      throw "Couldn't find info for guest code.";
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: error.statusCode || 404,
+      headers: {
+        ...headers,
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify({
+        message: `Couldn't find events for guestCode ${guestCode}. Please check the code again.`,
+      }),
+    };
+  }
 };

6fc1c7a0ff80965900b85fe95a57696e9ad14025
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-08 21:29:25 -0400

    getGuestRSVP endpoint created

---
+++ api/getGuestRSVP.js
@@ -0,0 +1,61 @@
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const { headers, dynamoTableNames } = require("../config");
+
+module.exports.handler = async (event) => {
+  const { firstName, lastName } = event.queryStringParameters;
+
+//   Check to see if first and last name are not empty
+  if (!firstName.length || !lastName.length) {
+    return {
+      statusCode: 400,
+      headers: {
+        ...headers,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Missing entries for firstName or lastName or both",
+      }),
+    };
+  }
+
+  let guestId;
+  guestId = firstName.charAt(0).toLowerCase() + lastName;
+  console.log(`Getting RSVP record for ${guestId}`);
+
+  const params = {
+    TableName: dynamoTableNames.Guests,
+    Key: {
+      guestId,
+      SK: firstName,
+    },
+  };
+
+  try {
+    let result = await dynamoDb.get(params).promise();
+    const { Item } = result;
+
+    // If the user enters a incorrect first and lastname combination but the guestId matches an entry, error should be thrown
+    if (!Object.keys(Item).length) {
+      throw `Couldn't find RSVP record for ${firstName} ${lastName}`;
+    }
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(Item),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: error.statusCode || 404,
+      headers: {
+        ...headers,
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify({
+        message: `Couldn't find RSVP record for ${firstName} ${lastName}`,
+      }),
+    };
+  }
+};
---
+++ api/updateRSVP.js
@@ -0,0 +1,12 @@
+// const AWS = require('aws-sdk');
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const {
+//     headers,
+//     dynamoTableNames
+// } = require('../config');
+
+
+
+// module.exports.handler = async() => {
+
+// }

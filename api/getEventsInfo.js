79beafee7dfbbf215631e0c9add7b21f48ce15f1
omerzulfiqar <omerzulfiqar@gmail.com>
2021-10-10 04:08:12 -0400

    getEventsInfo change to get from query

--- api/getEventsInfo.js
+++ api/getEventsInfo.js
@@ -3,28 +3,32 @@ const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
+  // Add validation for this in UI
   const { guestCode } = event.pathParameters;
 
   const params = {
     TableName: dynamoTableNames.Events,
-    KeyConditionExpression: "guestCode = :guestCode",
-    ExpressionAttributeValues: {
-      ":guestCode": guestCode,
-    },
+    Key: { guestCode },
+    // KeyConditionExpression: "guestCode = :guestCode",
+    // ExpressionAttributeValues: {
+    //   ":guestCode": guestCode,
+    // },
   };
 
   try {
-    let result = await dynamoDB.query(params).promise();
-    const { Items } = result;
+    let result = await dynamoDB.get(params).promise()
+    const { Item } = result;
+    console.log(Item);
 
-    if (Items.length) {
+    // Add validation for this in UI
+    if (Object.keys(Item).length) {
       return {
         statusCode: 200,
-        body: JSON.stringify(Items),
+        body: JSON.stringify(Item),
         headers,
       };
     } else {
-      throw "Couldn't find info for guest code.";
+      throw new Error();
     }
   } catch (error) {
     console.error(error);
@@ -32,7 +36,7 @@ module.exports.handler = async (event) => {
       statusCode: error.statusCode || 404,
       headers: {
         ...headers,
-        "Content-Type": "text/plain",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         message: `Couldn't find events for guestCode ${guestCode}. Please check the code again.`,
